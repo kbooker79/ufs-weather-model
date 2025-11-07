0e492f466649d88e56b77265c36ada411045c268
Kris Booker <kristopher.booker@tomorrow.io>
2025-11-07 10:00:02 -0700

    Updating.

--- tests/ci/repo_check.sh
+++ tests/ci/repo_check.sh
@@ -17,6 +17,7 @@ get_shas () {
     echo $common $base_sha $workspace
     if [[ $common != $base_sha ]]; then
         printf "%s\n\n" "** $workspace **NOT** up to date"
+        printf "$common is not equal to $base_sha"
         flag_sync=false
     fi
     cd $cwd
